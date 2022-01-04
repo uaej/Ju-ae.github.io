d71c9336b8ba4042043ee6a833129ffd833d68bc
Ju-ae <qowndo@gmail.com>
2022-01-04 16:20:32 +0900

    Fix padding in background

--- momentum/js/background.js
+++ momentum/js/background.js
@@ -4,7 +4,7 @@ const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
 //window 크기에 맞춰 사진 비율 뱐경
-bgImage.width = window.innerWidth;
-bgImage.height = window.innerHeight + 5;
+bgImage.width = window.innerWidth + 10;
+bgImage.height = window.innerHeight + 10;
 document.body.appendChild(bgImage);
-document.body.classList.add("background"); 
+bgImage.classList.add("background"); 
