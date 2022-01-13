de276f9d42ca31391676b256b2dd8fbb4edbeb81
Ju-ae <qowndo@gmail.com>
2022-01-14 00:13:05 +0900

    Add new way to use useEffect

--- react-beginner/src/App.js
+++ react-beginner/src/App.js
@@ -1,11 +1,29 @@
 import Button from "./Button"
 import styles from "./App.module.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick=() => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  console.log("I run all the time");
+  useEffect(() =>{
+    console.log("call api");
+  }, []);
+ 
+  useEffect(()=>{
+   if(keyword !== "" && keyword.length > 4){
+    console.log("search for", keyword);
+   }
+  }, [keyword, counter]);
+
   return (
    <div>
-     <h1 className={styles.title}>Welcome back!</h1>
-     <Button text={"continue"}/>
+     <input value={keyword} onChange={onChange} type="text" placeholder="search here.. "/>
+     <h1>{counter}</h1>
+     <button onClick={onClick}>click me</button>
    </div>
   );
 }
