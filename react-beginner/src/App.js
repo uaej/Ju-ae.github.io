7a9add8387f9d211b27f7de6929db189b0054759
Ju-ae <qowndo@gmail.com>
2022-01-22 23:48:02 +0900

    change usd to coin

--- react-beginner/src/App.js
+++ react-beginner/src/App.js
@@ -3,7 +3,9 @@ import { useEffect, useState } from "react";
 function App(){
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
-
+  const [usd, setUsd] = useState(0);
+  const [coinId, setCoinId]= useState("");
+  const[result, setResult] = useState("");
   useEffect(() => {
     const url= "https://api.coinpaprika.com/v1/tickers";
     fetch(url)
@@ -13,17 +15,44 @@ function App(){
       setCoins(json);
     }
     );
-
   }, []);
+
+  const onSelect = (event)=>{
+    console.log("onselect");
+    console.log(event.target.value);
+    var id = event.target.value;
+    setCoinId(id);
+  }
+  const onChange=(event)=>{
+    setUsd(event.target.value); 
+  }
+  const calculate =(event)=>{
+    event.preventDefault();
+    const data = coins.find(ele => ele.id == coinId);
+    let val = 0;
+    if(usd > 0){
+      val = usd / data.quotes.USD.price;
+    }
+    setResult(val + "   " + data.name + "(" + data.symbol +")");
+  }
   
   return(
     <div>
       <h1> The Coins{loading? "" : `(${coins.length})`}</h1>
+      <form onSubmit={calculate}>
+        <input onChange={onChange} type="number"></input>
+        USD   
+        <button>조회</button>
+        </form>
+      <br/>
       {loading ? <strong>Loading...</strong>
         : 
-        <select>
-          {coins.map((coin)=><option>{coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD</option>)}
+        <div>
+        <select onChange={onSelect}>
+          {coins.map((coin)=><option value={coin.id}>{coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD</option>)}
         </select>
+          <h2>{result}</h2>
+        </div>
       }
     </div>
   );
